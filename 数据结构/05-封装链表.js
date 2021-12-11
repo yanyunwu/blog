7e28abe04181e87b48b9eeb85cc568f055aae90e
烟云 <2480721346@qq.com>
2021-12-11 15:34:29 +0800

    链表

--- 数据结构/05-封装链表.js
+++ 数据结构/05-封装链表.js
@@ -1,13 +1,45 @@
 class Node {
-    constructor(value, next) {
-        this.value = value;
-        this.next = next || null;
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
 
 class NodeList {
     constructor() {
+        this.head = null;
+        this.length = 0;
+    }
 
+    append(data) {
+        const node = new Node(data);
+        if (this.head) {
+            let temp = this.head;
+            while (temp.next) {
+                temp = temp.next;
+            }
+            temp.next = node
+        } else {
+            this.head = node;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    toString() {
+        const head = this.head;
+        const list = [];
+        let temp = head;
+        while (temp) {
+            list.push(temp.data)
+            temp = temp.next;
+        }
+        return list;
     }
 }
+
+const list = new NodeList();
+list.append(1).append(2).append(3)
+console.log(list.toString());
+console.log(list.length);
--- 数据结构/06-双向链表.js
+++ 数据结构/06-双向链表.js
@@ -1,15 +1,78 @@
 class Node {
-    constructor() {
-        this.pre = null;
+    constructor(data) {
+        this.prev = null;
         this.next = null;
-        this.data = null;
+        this.data = data;
     }
+}
+
+class myError extends Error {
+
 }
 
 class LinkedList {
     constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
+    append(data) {
+        const node = new Node(data);
+        if (this.head) {
+            this.tail.next = node;
+            this.tail = node;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    insert(position, data) {
+        if (position < 0 || position > this.length) {
+            throw new myError('越界!');
+        }
+
+        const node = new Node(data);
+        if (!this.length) {
+            this.head = node;
+        } else if (!position) {
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+        } else {
+            let temp = this.head;
+            let tail = this.head.next;
+            for (let i = 1; i < position; i++) {
+                temp = temp.next;
+                tail = temp.next;
+            }
+            node.prev = temp;
+            node.next = tail;
+            temp.next = node;
+            tail && (tail.prev = node);
+        }
+        this.length += 1;
+    }
+
+    toString() {
+        const head = this.head;
+        const list = [];
+        let temp = head;
+        while (temp) {
+            list.push(temp.data)
+            temp = temp.next;
+        }
+        return list;
     }
 }
 
 const list = new LinkedList();
+list.append(1).append(2).append(3)
+list.insert(0, '首位');
+list.insert(3, "哈哈哈");
+console.log(list.toString());
+console.log(list.length);
+// console.log(list);
